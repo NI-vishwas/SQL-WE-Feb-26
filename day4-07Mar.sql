949c395490492f36866b672fe31e9805080a4f51
Vishwas <vishwasks.reach@gmail.com>
2026-03-07 23:34:41 +0530

    Add Northwind database commands and queries
    Added SQL commands for Northwind database operations including data selection and filtering.

--- day4-07Mar.sql
+++ day4-07Mar.sql
@@ -10,3 +10,31 @@ GRANT SELECT ON demo.* TO 'user1'@'localhost';
 FLUSH PRIVILEGES;
 -- Revoking the permissions
 REVOKE SELECT ON demo.* FROM 'user1'@'localhost';
+
+-- Northwind database commands
+USE Northwind;
+SHOW TABLES;
+
+-- SEEING ALL THE DATA IN A TABLE
+SELECT * FROM Employees;
+SELECT * FROM Customers;
+-- Selected ccolumns
+SELECT EmployeeID, FirstName, LastName FROM Employees;
+-- Row wise filtering
+-- filter based on exact values
+-- Get all the customers from london
+SELECT * FROM Customers 
+WHERE City = 'London';
+-- Get the customers whose name start from A
+-- _ for any single character
+-- % for any number of characters
+
+SELECT CustomerName FROM Customers
+WHERE CustomerName LIKE 'A%';
+-- get all the customers who have second letter 'n' in their name
+SELECT CustomerName FROM Customers
+WHERE CustomerName LIKE '_n%';
+
+SELECT CustomerName FROM Customers
+WHERE CustomerName LIKE '%n%';
+
